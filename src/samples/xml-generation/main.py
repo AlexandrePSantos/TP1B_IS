68f08dda23ec57bb08776cdab41a3b2e7018f593
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-11-06 10:14:17 +0000

    Update main.py

--- src/samples/xml-generation/main.py
+++ src/samples/xml-generation/main.py
@@ -3,3 +3,6 @@ from csv_to_xml_converter import CSVtoXMLConverter
 if __name__ == "__main__":
     converter = CSVtoXMLConverter("/data/sample_dataset.csv")
     print(converter.to_xml_str())
+    
+    # converter = CSVtoXMLConverter("/data/carprice_dataset.csv")
+    # print(converter.to_xml_str())
