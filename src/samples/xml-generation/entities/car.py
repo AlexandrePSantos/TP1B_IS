76ab6449ba1e8590fff061911102b4af622a5a95
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-11-14 16:27:31 +0000

    txt

--- src/samples/xml-generation/entities/car.py
+++ src/samples/xml-generation/entities/car.py
@@ -4,21 +4,22 @@ import xml.etree.ElementTree as ET
 
 class Car:
 
-    def __init__(self, name):
+    def __init__(self, company, model, city):
         Car.counter += 1
         self._id = Car.counter
-        self._name = name
+        self._company = company
+        self._model = model
+        self._city = city
             
-    # def to_xml(self):
-    #     el = ET.Element("Country")
-    #     el.set("id", str(self._id))
-    #     el.set("name", self._name)
-    #     return el
+    def to_xml(self):
+        el = ET.Element("Car")
+        el.set("id", str(self._id))
+        el.set("name", self._name)
+        el.set("age", self._age)
+        el.set("location_ref", str(self._city.get_id()))
+        return el
 
-    # def get_id(self):
-    #     return self._id
-
-    # def __str__(self):
-    #     return f"name: {self._name}, id:{self._id}"
+    def __str__(self):
+        return f"{self._company}, age:{self._model}, country:{self._city}"
     
 Car.counter = 0
--- src/samples/xml-generation/entities/city.py
+++ src/samples/xml-generation/entities/city.py
@@ -8,17 +8,69 @@ class City:
         City.counter += 1
         self._id = City.counter
         self._name = name
-            
-    # def to_xml(self):
-    #     el = ET.Element("Country")
-    #     el.set("id", str(self._id))
-    #     el.set("name", self._name)
-    #     return el
 
-    # def get_id(self):
-    #     return self._id
+    def to_xml(self):
+        el = ET.Element("City")
+        el.set("id", str(self._id))
+        el.set("name", self._name)
+        return el
 
-    # def __str__(self):
-    #     return f"name: {self._name}, id:{self._id}"
-    
+    def get_id(self):
+        return self._id
+
+    def __str__(self):
+        return f"name: {self._name}, id:{self._id}"
+
+
 City.counter = 0
+
+# <CarData>
+#   <Cars>
+#     <Car id="0">
+#       <CarCompany id="1">
+#         <CompanyName>Toyota</CompanyName>
+#       </CarCompany>
+#       <CarModel id="2">
+#         <ModelName>Vitz</ModelName>
+#       </CarModel>
+#     </Car>
+#   </Cars>
+
+#   <Location>
+#     <CarLocation id="3">
+#       <!-- Location Information -->
+#       <City>Islamabad</City>
+#       <!-- ... (Other location details) -->
+#     </CarLocation>
+#   </Location>
+
+#   <CarState>
+#     <CarStateInfo id="4">
+#       <!-- Car State Information -->
+#       <Price>2385000</Price>
+#       <Mileage>9869</Mileage>
+#       <RegistrationStatus>Un-Registered</RegistrationStatus>
+#       <ModelYear>2017</ModelYear>
+#       <!-- ... (Other car state details) -->
+#     </CarStateInfo>
+#   </CarState>
+
+#   <EngineInfo>
+#     <CarEngineInfo id="5">
+#       <!-- Engine Information -->
+#       <EngineType>Petrol</EngineType>
+#       <EngineCapacity>1000</EngineCapacity>
+#       <!-- ... (Other engine details) -->
+#     </CarEngineInfo>
+#   </EngineInfo>
+
+#   <AppearanceInfo>
+#     <CarAppearanceInfo id="6">
+#       <!-- Appearance Information -->
+#       <Color>Silver</Color>
+#       <BodyType>Hatchback</BodyType>
+#       <TransmissionType>Automatic</TransmissionType>
+#       <!-- ... (Other appearance details) -->
+#     </CarAppearanceInfo>
+#   </AppearanceInfo>
+# </CarData>
