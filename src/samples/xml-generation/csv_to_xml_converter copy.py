bafb405ea955d325eb92393a2862b54ae4629d04
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-11-14 21:48:12 +0000

    Update csv_to_xml_converter copy.py

--- src/samples/xml-generation/csv_to_xml_converter copy.py
+++ src/samples/xml-generation/csv_to_xml_converter copy.py
@@ -16,17 +16,45 @@ class CSVtoXMLConverter:
 
     def to_xml(self):   
         # read car
+        cafvs = self._reader.read_entities(
+            attr="Clean Alternative Fuel Vehicle (CAFV) Eligibility",
+            builder=lambda row: Cafv(row["Clean Alternative Fuel Vehicle (CAFV) Eligibility"])
+        )
+        
         # read location
+        cafvs = self._reader.read_entities(
+            attr="Clean Alternative Fuel Vehicle (CAFV) Eligibility",
+            builder=lambda row: Cafv(row["Clean Alternative Fuel Vehicle (CAFV) Eligibility"])
+        )
+        
         # read utility
+        cafvs = self._reader.read_entities(
+            attr="Clean Alternative Fuel Vehicle (CAFV) Eligibility",
+            builder=lambda row: Cafv(row["Clean Alternative Fuel Vehicle (CAFV) Eligibility"])
+        )
+        
         # read cafv
         cafvs = self._reader.read_entities(
             attr="Clean Alternative Fuel Vehicle (CAFV) Eligibility",
             builder=lambda row: Cafv(row["Clean Alternative Fuel Vehicle (CAFV) Eligibility"])
         )
 
-
         # generate the final xml
+        root_el = ET.Element("EletricCars")
 
+        teams_el = ET.Element("Teams")
+        for team in teams.values():
+            teams_el.append(team.to_xml())
+
+        countries_el = ET.Element("Countries")
+        for country in countries.values():
+            countries_el.append(country.to_xml())
+
+        root_el.append(teams_el)
+        root_el.append(countries_el)
+
+        return root_el
+    
     def to_xml_str(self):
         xml_str = ET.tostring(self.to_xml(), encoding='utf8', method='xml').decode()
         dom = md.parseString(xml_str)
