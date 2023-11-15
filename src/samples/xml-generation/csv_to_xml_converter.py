10b89cd0dc81e0d21e97aa79194708a91de67fce
Rui Alves <ruijorge02012001@gmail.com>
2023-11-15 16:24:04 +0000

    upgrade

--- src/samples/xml-generation/csv_to_xml_converter.py
+++ src/samples/xml-generation/csv_to_xml_converter.py
@@ -3,38 +3,40 @@ import xml.dom.minidom as md
 import xml.etree.ElementTree as ET
 from csv_reader import CSVReader
 
+# Importando classes do módulo entities.sample
 from entities.sample.country import Country
 from entities.sample.team import Team
 from entities.sample.player import Player
 
-# Main CSV
+# Importando classes do módulo entities
 from entities.car import Car
-from entities.charateristic import Charateristic
+from entities.characteristic import Characteristic
 from entities.city import City
 from entities.state import State
+
 
 class CSVtoXMLConverter:
 
     def __init__(self, path):
+        # Inicializando o leitor de CSV com o caminho fornecido
         self._reader = CSVReader(path)
 
     def to_xml(self):
-        # read countries
+        # Ler informações sobre países
         countries = self._reader.read_entities(
             attr="nationality",
             builder=lambda row: Country(row["nationality"])
         )
 
-        # read teams
+        # Ler informações sobre equipas (times)
         teams = self._reader.read_entities(
             attr="Current Club",
             builder=lambda row: Team(row["Current Club"])
         )
 
-        # read players
-
+        # Ler informações sobre jogadores
         def after_creating_player(player, row):
-            # add the player to the appropriate team
+            # Adicionar o jogador à equipa apropriada
             teams[row["Current Club"]].add_player(player)
 
         self._reader.read_entities(
@@ -47,24 +49,29 @@ class CSVtoXMLConverter:
             after_create=after_creating_player
         )
 
-        # generate the final xml
+        # Gerar o XML final
         root_el = ET.Element("Football")
 
+        # Elemento para armazenar informações sobre as equipas
         teams_el = ET.Element("Teams")
         for team in teams.values():
             teams_el.append(team.to_xml())
 
+        # Elemento para armazenar informações sobre os países
         countries_el = ET.Element("Countries")
         for country in countries.values():
             countries_el.append(country.to_xml())
 
+        # Adicionar elementos ao elemento raiz
         root_el.append(teams_el)
         root_el.append(countries_el)
 
         return root_el
 
     def to_xml_str(self):
-        xml_str = ET.tostring(self.to_xml(), encoding='utf8', method='xml').decode()
+        # Converter a estrutura XML para uma string formatada
+        xml_str = ET.tostring(
+            self.to_xml(), encoding='utf8', method='xml').decode()
         dom = md.parseString(xml_str)
         return dom.toprettyxml()
 
@@ -118,6 +125,5 @@ class CSVtoXMLConverter:
 
     #     root_el.append(teams_el)
     #     root_el.append(countries_el)
-    
+
     # ...
-    
--- src/samples/xml-generation/entities/sample/country.py
+++ src/samples/xml-generation/entities/sample/country.py
@@ -1,24 +1,31 @@
 import xml.etree.ElementTree as ET
 
-
+# Definindo a classe Country
 class Country:
 
+    # Inicializador da classe
     def __init__(self, name):
+        # Incrementa o contador de instâncias de Country
         Country.counter += 1
+        # Atribui um ID único baseado no contador
         self._id = Country.counter
+        # Atribui o nome do país
         self._name = name
 
+    # Método para gerar um elemento XML representando o país
     def to_xml(self):
         el = ET.Element("Country")
         el.set("id", str(self._id))
         el.set("name", self._name)
         return el
 
+    # Método para obter o ID do país
     def get_id(self):
         return self._id
 
+    # Método especial para representação em string do objeto Country
     def __str__(self):
         return f"name: {self._name}, id:{self._id}"
 
-
+# Inicializando o contador de instâncias de Country
 Country.counter = 0
