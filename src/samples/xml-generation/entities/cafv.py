7d4559998d1a689421eecbe5958d1a3ec29f5e7b
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-11-18 17:25:20 +0000

    Update cafv.py

--- src/samples/xml-generation/entities/cafv.py
+++ src/samples/xml-generation/entities/cafv.py
@@ -1,16 +1,35 @@
 # CAFV Eligibility
 import xml.etree.ElementTree as ET
 
+class CafvEligibilityCollection:
 
-class Cafv:
+    def __init__(self):
+        self._eligibilities = []
+
+    def add_eligibility(self, eligibility_name):
+        eligibility = CafvEligibility(eligibility_name)
+        self._eligibilities.append(eligibility)
+        return eligibility
+
+    def to_xml(self):
+        cafv_eligibilities_el = ET.Element("CAFV Eligibility")
+
+        for eligibility in self._eligibilities:
+            cafv_eligibilities_el.append(eligibility.to_xml())
+
+        return cafv_eligibilities_el
+
+class CafvEligibility:
+
+    counter = 0
 
     def __init__(self, name):
-        Cafv.counter += 1
-        self._id = Cafv.counter
+        CafvEligibility.counter += 1
+        self._id = CafvEligibility.counter
         self._name = name
-            
+
     def to_xml(self):
-        el = ET.Element("Clean Alternative Fuel Vehicle (CAFV) Eligibility")
+        el = ET.Element("Eligibility")
         el.set("id", str(self._id))
         el.set("name", self._name)
         return el
@@ -21,4 +40,3 @@ class Cafv:
     def __str__(self):
         return f"name: {self._name}, id:{self._id}"
     
-Cafv.counter = 0
