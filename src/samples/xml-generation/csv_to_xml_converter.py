1530862616fd357847faa834bdd9c871b6939c7c
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-11-20 10:17:53 +0000

    converter

--- src/samples/xml-generation/csv_to_xml_converter.py
+++ src/samples/xml-generation/csv_to_xml_converter.py
@@ -3,11 +3,6 @@ import xml.dom.minidom as md
 import xml.etree.ElementTree as ET
 from csv_reader import CSVReader
 
-# Importando classes do módulo entities.sample
-from entities.sample.country import Country
-from entities.sample.team import Team
-from entities.sample.player import Player
-
 # Importando classes do módulo entities
 from entities.car import *
 from entities.cafv import *
@@ -18,65 +13,81 @@ from entities.utility import *
 class CSVtoXMLConverter:
 
     def __init__(self, path):
-        # Inicializando o leitor de CSV com o caminho fornecido
         self._reader = CSVReader(path)
 
     def to_xml(self):
-        # Ler informações sobre países
-        countries = self._reader.read_entities(
-            attr="nationality",
-            builder=lambda row: Country(row["nationality"])
-        )
-        
-        states = self._reader.read_entities(
-            attr="state",
-            builder=lambda row: State(row["state"]),
-        )
-        
-        cities = self._reader.read_entities(
-            attr="city",
-            builder=lambda row: City(row["city"]),
-            after_create=states[]
-            #adicionar a cidade perguntar diogo que ele ja nao sabia o que fazer
+        # Read locations
+        locations = self._reader.read_entities(
+            attr="Location",
+            builder=lambda row: Location(row["Location"])
         )
 
-        # Ler informações sobre equipas (times)
-        teams = self._reader.read_entities(
-            attr="Current Club",
-            builder=lambda row: Team(row["Current Club"])
+        # Read cafv
+        cafv_collection = self._reader.read_entities(
+            attr="CAFV Eligibility",
+            builder=lambda row: Cafv(row["CAFV Eligibility"])
         )
 
-        # Ler informações sobre jogadores
-        def after_creating_player(player, row):
-            # Adicionar o jogador à equipa apropriada
-            teams[row["Current Club"]].add_player(player)
+        # Read electric utilities
+        electric_utilities_collection = self._reader.read_entities(
+            attr="Electric Utility",
+            builder=lambda row: Utility(row["Electric Utility"])
+        )
 
-        self._reader.read_entities(
-            attr="full_name",
-            builder=lambda row: Player(
-                name=row["full_name"],
-                age=row["age"],
-                country=countries[row["nationality"]]
+        # Read cars
+        def after_creating_car(car, row):
+            # Assign references to location, cafv, and utility
+            car.set_location(locations[row["Location"]])
+            car.set_cafv(cafv_collection[row["CAFV Eligibility"]])
+            car.set_utility(electric_utilities_collection[row["Electric Utility"]])
+
+        cars = self._reader.read_entities(
+            attr="VIN",
+            builder=lambda row: Car(
+                vin=row["VIN"],
+                maker=row["Maker"],
+                model=row["Model"],
+                electric_type=row["Electric Type"],
+                model_year=row["Model Year"],
+                electric_range=row["Electric Range"]
             ),
-            after_create=after_creating_player
+            after_create=after_creating_car
         )
 
-        # Gerar o XML final
-        root_el = ET.Element("EletricCars")
+        # Generate the final XML
+        root_el = ET.Element("ElectricCars")
 
-        # Elemento para armazenar informações sobre as equipas
-        teams_el = ET.Element("Teams")
-        for team in teams.values():
-            teams_el.append(team.to_xml())
+        makers_el = ET.Element("Makers")
+        for maker in set(car.maker for car in cars.values()):
+            maker_el = ET.Element("Maker", id=str(len(makers_el)), name=maker)
+            models_el = ET.Element("Models")
 
-        # Elemento para armazenar informações sobre os países
-        countries_el = ET.Element("Countries")
-        for country in countries.values():
-            countries_el.append(country.to_xml())
+            for model in set(car.model for car in cars.values() if car.maker == maker):
+                model_el = ET.Element("Model", id=str(len(models_el)), name=model, Electric_Type="BEV")  # Assuming Electric_Type is constant for now
+                cars_el = ET.Element("Cars")
 
-        # Adicionar elementos ao elemento raiz
-        root_el.append(teams_el)
-        root_el.append(countries_el)
+                for car in cars.values():
+                    if car.maker == maker and car.model == model:
+                        car_el = car.to_xml()
+                        cars_el.append(car_el)
+
+                model_el.append(cars_el)
+                models_el.append(model_el)
+
+            maker_el.append(models_el)
+            makers_el.append(maker_el)
+
+        cafv_eligibility_el = cafv_collection.to_xml()
+        electric_utilities_el = electric_utilities_collection.to_xml()
+        locations_el = ET.Element("Locations")
+
+        for location in locations.values():
+            locations_el.append(location.to_xml())
+
+        root_el.append(makers_el)
+        root_el.append(cafv_eligibility_el)
+        root_el.append(electric_utilities_el)
+        root_el.append(locations_el)
 
         return root_el
 
--- src/samples/xml-generation/main.py
+++ src/samples/xml-generation/main.py
@@ -1,7 +1,6 @@
 from csv_to_xml_converter import CSVtoXMLConverter
 
 if __name__ == "__main__":
-    # converter = CSVtoXMLConverter("/data/carprice_dataset.csv")
-    converter = CSVtoXMLConverter("/data/sample_dataset.csv")
+    converter = CSVtoXMLConverter("/data/Electric_Vehicle_Population_Data.csv")
     print(converter.to_xml_str())
     
