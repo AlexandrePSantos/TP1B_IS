a7ba2fb5a6772005962cd46e34c179d374a57245
Rui Alves <ruijorge02012001@gmail.com>
2023-11-24 10:22:34 +0000

    Update main.py

--- src/rpc-client/main.py
+++ src/rpc-client/main.py
@@ -1,9 +1,35 @@
 import xmlrpc.client
 
-print("connecting to server...")
+print("Conectando ao servidor...")
 server = xmlrpc.client.ServerProxy('http://is-rpc-server:9000')
 
-string = "hello world"
+sair = True
+while sair:
+    print("\n\n--------MENU--------")
+    print("1 - Inverter String")
+    print("2 - Obter Comprimento da String")
+    print("3 - Listar Arquivos")
+    print("0 - Sair")
 
-print(f" > {server.string_reverse(string)}")
-print(f" > {server.string_length(string)}")
+    try:
+        opcao = input("Digite o número da opção desejada: ")
+
+        if opcao == "0":
+            sair = False
+        elif opcao == "1":
+            string = input("Digite a string a ser invertida: ")
+            resultado = server.string_reverse(string)
+            print(f"Resultado: {resultado}")
+        elif opcao == "2":
+            string = input("Digite a string para obter o comprimento: ")
+            resultado = server.string_length(string)
+            print(f"Resultado: {resultado}")
+        elif opcao == "3":
+            # Adicione aqui a lógica para listar arquivos, se necessário
+            pass
+        else:
+            print("Opção inválida. Tente novamente.")
+            
+    except Exception as e:
+        print(f"Erro: {e}")
+        sair = False
