82605a89d93f4d310af44962a65789a635801e3d
Rui Alves <ruijorge02012001@gmail.com>
2023-12-04 09:51:23 +0000

    upd

--- src/rpc-server/functions/query2.py
+++ src/rpc-server/functions/query2.py
@@ -2,4 +2,4 @@ from functions.exec_query import execute_query
 
 
 def num_car_Maker(maker_name):
-    return execute_query("WITH doc(file) AS (SELECT xml FROM imported_documents) SELECT xpath('count(//Car/Maker[.={0}])', file) FROM doc".format(maker_name))
+    return execute_query("WITH doc(file) AS (SELECT xml FROM imported_documents) SELECT xpath('count(//Maker/Model/Car[.={0}])', file) FROM doc".format(maker_name))
--- src/rpc-server/functions/query4.py
+++ src/rpc-server/functions/query4.py
@@ -1,5 +1,4 @@
 from functions.exec_query import execute_query
 
-
-def releases_from_car_by_Model(model_name):
-    return execute_query("WITH doc(file) AS (SELECT xml FROM imported_documents) SELECT xpath('//Model[@name={}]', file) FROM doc".format(model_name))
+def cars_to_year_and_eligibility(cars_year, eligibility):
+    return execute_query("WITH doc(file) AS (SELECT xml FROM imported_documents) SELECT xpath('//Model[@year<={} and @Eligibility={}]', file) FROM doc".format(cars_year, eligibility))
